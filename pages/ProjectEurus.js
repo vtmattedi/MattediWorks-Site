df7e593bee6aa8052a611314866466346992ed26
Vitor <vtmattediworks@gmail.com>
2021-06-03 01:29:03 -0300

    Title change

--- pages/ProjectEurus.js
+++ pages/ProjectEurus.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Eurus</title>
         <link rel="icon" href="/favicon.ico" />
         <script data-ad-client="ca-pub-4059701523804358" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
